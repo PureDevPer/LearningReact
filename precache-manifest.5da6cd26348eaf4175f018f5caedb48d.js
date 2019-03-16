06e16bb54d6bec950a9ce6569cbdb25cd0f23c97
PureDevPer <wooseok.kim1207@gmail.com>
2019-03-16 15:53:00 -0400

    Updates

---
+++ precache-manifest.5da6cd26348eaf4175f018f5caedb48d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LearningReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1777d9bb0f478ef608b4",
+    "url": "/LearningReact/static/js/runtime~main.7d7ad844.js"
+  },
+  {
+    "revision": "2857620ff0651c2a4bad",
+    "url": "/LearningReact/static/js/main.92309811.chunk.js"
+  },
+  {
+    "revision": "a35ee8e669eeb51c0deb",
+    "url": "/LearningReact/static/js/2.639102df.chunk.js"
+  },
+  {
+    "revision": "2857620ff0651c2a4bad",
+    "url": "/LearningReact/static/css/main.59ec65de.chunk.css"
+  },
+  {
+    "revision": "8c1e96ca1e2e9515ef73c8860d02a82e",
+    "url": "/LearningReact/index.html"
+  }
+];
